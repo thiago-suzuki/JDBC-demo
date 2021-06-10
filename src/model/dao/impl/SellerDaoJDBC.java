bb6a495cd9c3d17622e1a2bd687df7144b25f29b
Thiago Suzuki <thiagoikenaga@gmail.com>
2021-06-09 22:09:09 -0300

    Implementação da DaoFactory e SellerDaoJDBC

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Stub de método gerado automaticamente
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Stub de método gerado automaticamente
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Stub de método gerado automaticamente
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+}
